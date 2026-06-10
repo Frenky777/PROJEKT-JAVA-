619953d2ddc27e276a1bf3d01b5ae347a018c808
Frenky777 <fpakos5@gmail.com>
2026-06-10 16:58:34 +0200

    feat: implement book borrowing, returns, history and swagger auth config

---
+++ project/src/main/java/com/example/demo/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // logowanie http do swaggera
+                .components(new Components()
+                        .addSecuritySchemes("basicAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("basic")))
+                // ta klodka w swaggerze zeby moc sie bawic jako admin
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"));
+    }
+}
